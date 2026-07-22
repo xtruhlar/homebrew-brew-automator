f084b014d1be8a447cc7c7dec8a7d19d1e862a30
xtruhlar <xtruhlar@users.noreply.github.com>
2026-07-22 11:39:09 +0200

    Bump to v0.5.0 (optional SMTP config)

--- Formula/brew-automator.rb
+++ Formula/brew-automator.rb
@@ -1,8 +1,8 @@
 class BrewAutomator < Formula
   desc "Homebrew maintenance automation with SMTP email reports"
   homepage "https://github.com/xtruhlar/brew-automator"
-  url "https://github.com/xtruhlar/brew-automator/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "79f510640dcc0a4beb227ae9532b56307a395a84a9e7c4acc25a2e67dba58488"
+  url "https://github.com/xtruhlar/brew-automator/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "161ebd93b72e012fa7d9b8b34435c37606b64387cde773a80a3f09174afe33d4"
   license "MIT"
 
   depends_on "python@3.12"
